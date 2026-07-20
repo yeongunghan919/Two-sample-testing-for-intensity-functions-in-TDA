865a3cd0a98926cc90e2691c815179870164cbf9
YeongUngHan <duddnd919@snu.ac.kr>
2026-07-20 14:13:14 +0900

    Reorganize repository and exclude large data files

---
+++ Per_Image_testing/Torus_data_PI.R
@@ -0,0 +1,41 @@
+script_dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
+setwd(script_dir)
+source("functions.R")
+
+# settings
+res=40 # resolution 40 by 40
+rangex=c(0,2)
+rangey=c(0,2)
+h=0.15
+
+
+
+## load data
+torus_pd = readRDS("../data/Rdata/npc=50_nset=100torus_pd.Rdata")
+
+torus_pi_root = list()
+
+for (ii in 1:6){
+  torus_pi_root[[ii]] = list()
+  for (jj in 1:5000){
+    pd=torus_pd[[ii]][[jj]]
+    pdmat=data.frame(t(matrix(pd, nrow = 3, byrow = TRUE))) #revised version 
+    colnames(pdmat)=c("dimension","birth","death") 
+    pd1 = pdmat %>%  
+      dplyr::filter(dimension==1) %>%
+      dplyr::select(-dimension)
+    pd1$death = pd1$death - pd1$birth
+    
+    # weights
+    arctanweight = arctanw(pd1,0.5,0.5)
+    linearweight = linearw(pd1)
+    rootweight = polyweight(pd1,n=1/2)
+    
+    # compute persistence image
+    pi1_root=pers.image(pd=pd1, rangex=rangex, rangey=rangey, wgt=rep(1,nrow(pd1)), nbins=res, h=h)
+    torus_pi_root[[ii]][[jj]] = pi1_root
+  }
+}
+
+## save persistence images
+save(torus_pi_root, file=paste0("PI_Torus_data/data_pi_torus_root.Rdata"))
---
+++ Per_Image_testing/Torus_simulation.R
@@ -0,0 +1,46 @@
+source("functions.R")
+
+# settings
+sig=c(0.01,0.02,0.03,0.04,0.05)
+npc=50
+nset=100
+range=c(0,4)
+res=40
+alpha=0.05
+
+load("PI_Torus_data/data_pi_torus_root.Rdata")
+
+## Power.  
+torus_pi_root_noised = torus_pi_root[1:5]
+torus_pi_root_not_noised = rep(torus_pi_root[6],5)
+pvals_root_power=ts_main_power(torus_pi_root_noised,torus_pi_root_not_noised,sig,npc,nset,range,res,alpha)
+rowMeans(pvals_root_power[,,2]) #C=0.2 <-> ,,2 
+saveRDS(rowMeans(pvals_root_power[,,2]), "../results/simulation_results/Torus_results/Torus_PI_power_result.rds")
+
+
+
+###############################################################################
+
+# settings with different sample sizes
+sig =c(0.02)
+list_npc=c(20,40,70,100)
+nset=50
+range=c(0,4)
+res=40
+alpha=0.05
+
+load("PI_Torus_data/data_pi_torus_root.Rdata")
+
+## Power  
+torus_pi_root_noised = rep(torus_pi_root[2],1)
+torus_pi_root_not_noised = rep(torus_pi_root[6],1)
+pvals_root_power <- matrix(NA, nrow = length(list_npc), ncol =nset)
+for (n in 1:length(list_npc)){
+  npc=list_npc[n]
+  test_results=ts_main_power(torus_pi_root_noised,torus_pi_root_not_noised,sig,npc,nset,range,res,alpha)
+  pvals_root_power[n,]=test_results[,,2] #C=0.2 <-> ,,2 
+}
+
+rowMeans(pvals_root_power)
+saveRDS(rowMeans(pvals_root_power), "../results/simulation_results/Torus_results//Torus_PI_power_result_diffsamples.rds")
+
